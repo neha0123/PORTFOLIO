35be022b5dbd8b1aa739c6f33b73caac5118e792
Neha Garg <garg22neha1999@gmail.com>
2020-12-28 21:29:18 +0530

    add

--- js/main.js
+++ js/main.js
@@ -101,17 +101,6 @@
 		});
 	}
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
+	
 
 })(jQuery);
